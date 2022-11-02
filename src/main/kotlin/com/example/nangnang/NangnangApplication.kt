04da90dfce06ff0a2e736bc3aa0091d4f5699122
yumtam kim <yumtamking@gmail.com>
2022-11-03 01:12:35 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/nangnang/NangnangApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nangnang
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NangnangApplication
+
+fun main(args: Array<String>) {
+	runApplication<NangnangApplication>(*args)
+}
